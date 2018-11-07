a9e6b29adae00dbeec65c599fd29f829b7739295
ssywbj <1271829012@qq.com>
2018-11-08 01:13:42 +0800

    项目提交

---
+++ app/src/main/java/com/suheng/ssy/boutique/BoutiqueActivity.java
@@ -0,0 +1,13 @@
+package com.suheng.ssy.boutique;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoutiqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boutique);
+    }
+}
