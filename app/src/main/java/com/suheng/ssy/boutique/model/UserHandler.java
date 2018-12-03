2fde116c2db6a5ebed41d5128f255f38397915f4
ssywbj <1271829012@qq.com>
2018-12-04 01:10:01 +0800

    databinding

--- app/src/main/java/com/suheng/ssy/boutique/model/UserHandler.java
+++ app/src/main/java/com/suheng/ssy/boutique/model/UserHandler.java
@@ -1,5 +1,6 @@
 package com.suheng.ssy.boutique.model;
 
+import android.text.Editable;
 import android.util.Log;
 
 public class UserHandler {
@@ -29,4 +30,12 @@ public class UserHandler {
         Log.d(TAG, "name value = " + name + ", age = " + age);
     }
 
+    public void afterTextChanged(Editable s) {
+        mUser.setName(s.toString());
+        Log.d(TAG, "afterTextChanged = " + s.toString());
+    }
+
+    public void onClickSetNameNull() {
+        mUser.setName(null);//避免空指针异常：name为null的话，tmpUser.name会被赋值为默认值null，而不会抛出空指针异常
+    }
 }
---
+++ app/src/main/java/com/suheng/ssy/boutique/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package com.suheng.ssy.boutique.utils;
+
+public class StringUtils {
+
+    public static String format(String text) {
+        return text + "---";
+    }
+
+}
