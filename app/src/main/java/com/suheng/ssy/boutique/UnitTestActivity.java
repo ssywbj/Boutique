624fe67a6d79c78993cadd658970a63e52755344
Wbj <weibangjie@xunlei.com>
2019-01-03 21:25:23 +0800

    ScrollView中嵌套WebView时，导致 ScrollView自动滚动的问题

--- app/src/main/java/com/suheng/ssy/boutique/UnitTestActivity.java
+++ app/src/main/java/com/suheng/ssy/boutique/UnitTestActivity.java
@@ -1,7 +1,15 @@
 package com.suheng.ssy.boutique;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ScrollView;
 
 public class UnitTestActivity extends BasicActivity {
 
@@ -9,6 +17,36 @@ public class UnitTestActivity extends BasicActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unit_test);
+
+        WebView webView = findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.getSettings().setDefaultFontSize(18);
+        webView.setBackgroundColor(0);
+        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        webView.loadUrl("https://juejin.im/post/5b57e3fbf265da0f47352618");//加载远程网页
+        webView.setWebChromeClient(new WebChromeClient() {
+        });//保证能弹出网页的alert对话框
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            //网页加载失败会调用该方法
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+        });
+
+        ((ScrollView) findViewById(R.id.scroll_view)).smoothScrollTo(0, 0);
     }
 
 }
