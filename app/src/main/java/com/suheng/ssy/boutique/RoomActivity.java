5a34a79b85195fb08fbeb19c8ecd925fd9314c0a
ssywbj <1271829012@qq.com>
2019-03-27 01:52:32 +0800

    使用RxJava操作数据库

--- app/src/main/java/com/suheng/ssy/boutique/RoomActivity.java
+++ app/src/main/java/com/suheng/ssy/boutique/RoomActivity.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
@@ -30,39 +33,80 @@ public class RoomActivity extends BasicActivity {
     }
 
     public void onClickInsertOne(View view) {
-        Observable.just(new UserEntity(1210214142, "Wiii", "Bangjie"))
+        Observable.just(new UserEntity(1210214142, "Weeee", "Baaaajie"))
                 .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
                 .map(new Func1<UserEntity, Long>() {
                     @Override
                     public Long call(UserEntity userEntity) {
                         Long userId = mUserDao.insert(userEntity);
-                        Log.d(mTag, "InsertOne: " + userId);
+                        Log.d(mTag, "InsertOne, map: " + userId + ", " + Thread.currentThread().getName());
                         return userId;
                     }
                 })
-                .subscribe();
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        Log.d(mTag, "InsertOne, subscribe: " + Thread.currentThread().getName());
+                    }
+                });
     }
 
     public void onClickInsertList(View view) {
-        final List<UserEntity> userEntityList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        List<UserEntity> userEntityList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
             userEntityList.add(new UserEntity(1210214142 + i, "Wiii" + i, "Bangjeee" + i));
         }
+
         Observable.from(userEntityList)
                 .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .map(new Func1<UserEntity, List<Long>>() {
+                .map(new Func1<UserEntity, Long>() {
                     @Override
-                    public List<Long> call(UserEntity userEntity) {
-                        List<Long> userIdList = mUserDao.insert(userEntityList);
-                        for (Long userId : userIdList) {
-                            Log.d(mTag, "InsertList: " + userId);
-                        }
-                        return userIdList;
+                    public Long call(UserEntity userEntity) {
+                        Long userId = mUserDao.insert(userEntity);
+                        Log.d(mTag, "InsertList1, map: " + userId + ", " + Thread.currentThread().getName());
+                        return userId;
                     }
                 })
-                .subscribe();
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long userId) {
+                        Log.d(mTag, "InsertList1, subscribe: " + Thread.currentThread().getName() + ", " + userId);
+                    }
+                });
+
+        Observable
+                .create(new Observable.OnSubscribe<List<UserEntity>>() {
+                    @Override
+                    public void call(Subscriber<? super List<UserEntity>> subscriber) {
+                        List<UserEntity> entityList = new ArrayList<>();
+                        for (int i = 0; i < 2; i++) {
+                            entityList.add(new UserEntity(221021442 + i, "Wccc" + i, "Bhhhhhh" + i));
+                        }
+                        subscriber.onNext(entityList);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .map(new Func1<List<UserEntity>, List<Long>>() {
+                    @Override
+                    public List<Long> call(List<UserEntity> userEntityList) {
+                        return mUserDao.insert(userEntityList);
+                    }
+                })
+                .filter(new Func1<List<Long>, Boolean>() {
+                    @Override
+                    public Boolean call(List<Long> longs) {
+                        return (longs != null);
+                    }
+                })
+                .subscribe(new Action1<List<Long>>() {
+                    @Override
+                    public void call(List<Long> longs) {
+                        for (Long userId : longs) {
+                            Log.i(mTag, "InsertList2, thread: " + Thread.currentThread().getName() + ", " + userId);
+                        }
+                    }
+                });
     }
 
     public void onClickQueryOne(View view) {
@@ -76,6 +120,30 @@ public class RoomActivity extends BasicActivity {
     }
 
     public void onClickQueryList(View view) {
+        Observable
+                .create(new Observable.OnSubscribe<List<UserEntity>>() {
+                    @Override
+                    public void call(Subscriber<? super List<UserEntity>> subscriber) {
+                        Log.d(mTag, "QueryList, create: " + Thread.currentThread().getName());
+                        subscriber.onNext(mUserDao.getUserList());
+                    }
+                })
+                .filter(new Func1<List<UserEntity>, Boolean>() {
+                    @Override
+                    public Boolean call(List<UserEntity> userEntityList) {
+                        Log.d(mTag, "QueryList, filter: " + Thread.currentThread().getName());
+                        return (userEntityList != null);
+                    }
+                }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<UserEntity>>() {
+                    @Override
+                    public void call(List<UserEntity> userEntityList) {
+                        for (UserEntity userEntity : userEntityList) {
+                            Log.d(mTag, "QueryList, thread: " + Thread.currentThread().getName() + ", " + userEntity);
+                        }
+                    }
+                });
 
     }
 
