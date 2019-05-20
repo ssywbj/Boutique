771937a8d0bae8d3af21eaed5cd27db3bdbc78cd
ssywbj <1271829012@qq.com>
2019-05-21 01:07:53 +0800

    探究键盘属性的使用

--- app/src/main/java/com/suheng/ssy/boutique/KeyboardActivity.java
+++ app/src/main/java/com/suheng/ssy/boutique/KeyboardActivity.java
@@ -12,7 +12,11 @@ import com.suheng.ssy.boutique.model.Constants;
 @Route(path = Constants.ROUTER_APP_ACTIVITY_KEYBOARD)//路径至少需要有两级，第一级是Group名称
 public class KeyboardActivity extends BasicActivity {
 
+    public static final int ALPHABET = -10;//字母表
+    public static final int PUNCTUATIONS = -12;//标点符号集
+
     @Override
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyboard);
@@ -20,10 +24,15 @@ public class KeyboardActivity extends BasicActivity {
         KeyboardView keyboardView = findViewById(R.id.keyboard);
         Keyboard keyboard = new Keyboard(this, R.xml.keyboard_number);
         keyboardView.setKeyboard(keyboard);
+        keyboardView.setPreviewEnabled(false);//回显，默认为true
         keyboardView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
             @Override
-            public void onPress(int primaryCode) {
-                Log.d(mTag, "onPress, primaryCode = " + primaryCode);
+            public void onPress(int primaryCode) {//只要设置了keyOutputText属性，输出primaryCode都为-1
+                if (primaryCode == ALPHABET) {
+                    Log.d(mTag, "onPress, primaryCode = " + primaryCode + ", alphabet");
+                } else if (primaryCode == PUNCTUATIONS) {
+                    Log.d(mTag, "onPress, primaryCode = " + primaryCode + ", punctuations");
+                }
             }
 
             @Override
@@ -33,11 +42,11 @@ public class KeyboardActivity extends BasicActivity {
 
             @Override
             public void onKey(int primaryCode, int[] keyCodes) {
-                Log.d(mTag, "onKey, primaryCode = " + primaryCode + ", keyCodes = " + keyCodes.length);
+                Log.d(mTag, "onKey, primaryCode = " + primaryCode + ", keyCodes.length = " + keyCodes.length);
             }
 
             @Override
-            public void onText(CharSequence text) {
+            public void onText(CharSequence text) {//配合keyOutputText使用，有则调用，没有则不调用
                 Log.d(mTag, "onText, text = " + text);
             }
 
--- app/src/main/java/com/suheng/ssy/boutique/model/Constants.java
+++ app/src/main/java/com/suheng/ssy/boutique/model/Constants.java
@@ -2,5 +2,5 @@ package com.suheng.ssy.boutique.model;
 
 public class Constants {
 
-    public static final String ROUTER_APP_ACTIVITY_KEYBOARD = "/app/activity/keyboard";//module/组件/功能
+    public static final String ROUTER_APP_ACTIVITY_KEYBOARD = "/app/activity/keyboard";//格式：/module/组件/功能
 }
