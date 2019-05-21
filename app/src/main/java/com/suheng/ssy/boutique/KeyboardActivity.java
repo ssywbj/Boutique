df8d87b9d88e2a8b55e543cffe5b86fb8dedd9dc
ssywbj <1271829012@qq.com>
2019-05-22 03:35:34 +0800

    自定义键盘长按事件

--- app/src/main/java/com/suheng/ssy/boutique/KeyboardActivity.java
+++ app/src/main/java/com/suheng/ssy/boutique/KeyboardActivity.java
@@ -4,7 +4,9 @@ import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.util.Log;
+import android.widget.EditText;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.suheng.ssy.boutique.model.Constants;
@@ -13,7 +15,7 @@ import com.suheng.ssy.boutique.model.Constants;
 public class KeyboardActivity extends BasicActivity {
 
     public static final int ALPHABET = -10;//字母表
-    public static final int PUNCTUATIONS = -12;//标点符号集
+    //public static final int PUNCTUATIONS = -12;//标点符号集
 
     @Override
 
@@ -21,33 +23,41 @@ public class KeyboardActivity extends BasicActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_keyboard);
 
+        final EditText editNumber = findViewById(R.id.edit_number);
+        /*InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(editNumber.getWindowToken(), 0);*/
         KeyboardView keyboardView = findViewById(R.id.keyboard);
         Keyboard keyboard = new Keyboard(this, R.xml.keyboard_number);
         keyboardView.setKeyboard(keyboard);
         keyboardView.setPreviewEnabled(false);//回显，默认为true
         keyboardView.setOnKeyboardActionListener(new KeyboardView.OnKeyboardActionListener() {
             @Override
-            public void onPress(int primaryCode) {//只要设置了keyOutputText属性，输出primaryCode都为-1
-                if (primaryCode == ALPHABET) {
-                    Log.d(mTag, "onPress, primaryCode = " + primaryCode + ", alphabet");
-                } else if (primaryCode == PUNCTUATIONS) {
-                    Log.d(mTag, "onPress, primaryCode = " + primaryCode + ", punctuations");
-                }
+            public void onPress(int primaryCode) {//设置isRepeatable为true的按键长按后只会执行一次onPress方法，然后重复执行onKey、onRelease方法
+                Log.d(mTag, "onPress, primaryCode = " + primaryCode);
             }
 
             @Override
-            public void onRelease(int primaryCode) {
+            public void onRelease(int primaryCode) {//只要设置keyOutputText属性，输出primaryCode都为-1
                 Log.d(mTag, "onRelease, primaryCode = " + primaryCode);
             }
 
             @Override
-            public void onKey(int primaryCode, int[] keyCodes) {
-                Log.d(mTag, "onKey, primaryCode = " + primaryCode + ", keyCodes.length = " + keyCodes.length);
+            public void onKey(int primaryCode, int[] keyCodes) {//只要设置keyOutputText属性，就不会执行该方法，而是执行onText方法
+                if (primaryCode == ALPHABET) {
+                    Log.d(mTag, "onKey, primaryCode = " + primaryCode + ", alphabet" + ", keyCodes.length = " + keyCodes.length);
+                } else if (primaryCode == Keyboard.KEYCODE_DELETE) {
+                    Log.d(mTag, "onKey, primaryCode = " + primaryCode + ", delete" + ", keyCodes.length = " + keyCodes.length);
+                    Editable editable = editNumber.getText();
+                    if (editable.length() > 0) {
+                        editable.delete(editable.length() - 1, editable.length());
+                    }
+                }
             }
 
             @Override
             public void onText(CharSequence text) {//配合keyOutputText使用，有则调用，没有则不调用
                 Log.d(mTag, "onText, text = " + text);
+                editNumber.getText().insert(editNumber.getSelectionStart(), text);
             }
 
             @Override
